0c5a4743f2c58e5af62689bd1c5f846442e9b8bb
saqeebkhan <shakibkhan8271@gmail.com>
2021-03-07 21:05:10 +0530

    FirstProgram

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter Name");
+//        String name= sc.nextLine();
+//        System.out.println("Hello"+"\n"+name);
+
+
+
+
+
+
+
+    }
+}
